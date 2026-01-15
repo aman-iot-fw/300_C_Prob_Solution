27cc6539fa3eaa2f26e97b7f01ea083fcd315066
aman-iot-fw <aman.12006417@gmail.com>
2026-01-15 19:49:44 +0530

    add solution in string basic

--- 01_strings/basic/01_print_duplicate_characters.c
+++ 01_strings/basic/01_print_duplicate_characters.c
@@ -1,1 +1,21 @@
-// TODO
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+uint8_t freq[256] = {0};
+char *str = "Helloo";
+
+int main(){
+    uint8_t len = strlen(str);
+    
+    for(uint8_t i = 0; i < len; i++){
+        freq[str[i]]++;
+    }
+
+    for(uint8_t i = 0; i < 255; i++){
+        if(freq[i] > 1){
+            printf("%c ", i);
+        }
+    }
+    return 0;
+}
