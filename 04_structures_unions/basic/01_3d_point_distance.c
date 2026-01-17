b77027352be55520804a46f988fa7f21fb3be33a
aman-iot-fw <aman.12006417@gmail.com>
2026-01-17 15:33:36 +0530

    solution added

--- 04_structures_unions/basic/01_3d_point_distance.c
+++ 04_structures_unions/basic/01_3d_point_distance.c
@@ -1,1 +1,32 @@
-// TODO: Implement structure/union based solution
+#include <stdio.h>
+#include <math.h>
+
+
+struct point3D
+{
+    double x;
+    double y;
+    double z;
+};
+
+double distanceBetweenPoints(const struct point3D *p1, const struct point3D *p2){
+    //To calculate distance use formula -  sqrt(((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)) + ((z2-z1)*(z2-z1)))
+    double dx = (p2->x - p1->x);
+    double dy = (p2->y - p1->y);
+    double dz = (p2->z - p1->z);
+    
+    double distance = sqrt((dx*dx) + (dy*dy) + (dz*dz));
+    return distance;
+}
+
+
+int main(void){
+    struct point3D p1 = {2, 4, 6}; 
+    struct point3D p2 = {1, 3, 5}; 
+
+    double distance  = distanceBetweenPoints(&p1, &p2);
+    printf("Distance between point : %.3f\n", distance);
+
+
+    return 0;
+}
--- 05_linked_list/basic/01_find_middle_element.c
+++ 05_linked_list/basic/01_find_middle_element.c
@@ -4,13 +4,42 @@
 
 struct List
 {
+    struct List *next;
     int data;
-    struct List *Next;
 };
 
+
 int main(){
-
-    printf("%d", sizeof(struct List));
-
+    
+    struct List *node1 = (struct List*)malloc(sizeof(struct List));
+    struct List *node2 = (struct List*)malloc(sizeof(struct List));
+    struct List *node3 = (struct List*)malloc(sizeof(struct List));
+    struct List *node4 = (struct List*)malloc(sizeof(struct List));
+    struct List *node5 = (struct List*)malloc(sizeof(struct List));
+    
+    node1->data = 10;
+    node1->next = node2;
+    node2->data = 20;
+    node2->next = node3;
+    node3->data = 30;
+    node3->next = node4;
+    node4->data = 40;
+    node4->next = node5;
+    node5->data = 50;
+    node5->next = NULL;
+    
+    struct List *temp = node1;
+    static uint8_t count =0;
+    while(temp != NULL){
+        count +=1;
+        temp = temp->next;
+    }
+    
+    temp = node1;
+    for(uint8_t i = 0; i < (count/2); i++){
+        temp = temp->next;
+    }
+    printf("Mid element : %d",temp->data );
+    
     return 0;
 }
