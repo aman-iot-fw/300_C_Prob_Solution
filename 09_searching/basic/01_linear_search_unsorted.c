a6f8bd5d6494a52e8eec00efc0d6ef6b351ae1ec
aman-iot-fw <aman.12006417@gmail.com>
2026-01-29 23:35:12 +0530

    Solution added

--- 09_searching/basic/01_linear_search_unsorted.c
+++ 09_searching/basic/01_linear_search_unsorted.c
@@ -1,1 +1,28 @@
-// TODO: Implement searching logic
+#include<stdio.h>
+
+int searchInex(int arr[], int size, int target ){
+    for(int i =0; i < size; i++){
+        if(arr[i] == target){
+            return i;
+        }
+    }
+    return -1;
+
+}
+
+int main(){
+    int arr[] = {2,4,6,3,8,9};
+    size_t size = sizeof(arr)/sizeof(arr[0]);
+    int target = 9;
+
+    int index = searchInex(arr, size, target);
+
+    if(index != -1){
+        printf("Index of the target number in array: %d\n", index);
+    }
+    else{
+        printf("Target not found in the given array");
+    }
+
+    return 0;
+}
