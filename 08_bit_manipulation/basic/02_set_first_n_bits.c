67dbeb39de8a0c5a673334bd6da3119df253a956
aman-iot-fw <aman.12006417@gmail.com>
2026-02-02 10:58:37 +0530

    solution added

--- 08_bit_manipulation/basic/02_set_first_n_bits.c
+++ 08_bit_manipulation/basic/02_set_first_n_bits.c
@@ -1,1 +1,19 @@
-// TODO: Implement bit manipulation logic
+#include<stdio.h>
+#include<stdint.h>
+
+uint8_t setNbits(uint8_t inp, uint8_t n){
+    if(n > 8){
+      n = 8;  
+    } 
+    if(n == 0){
+      return inp;  
+    } 
+    uint8_t mask = (1u << n) - 1;
+    return inp |= mask;
+}
+
+int main(){
+    uint8_t oneByte = 0b00000000;
+    printf("%u (Binary 0b%08b)", setNbits(oneByte, 2),setNbits(oneByte, 2));
+    return 0;
+}
