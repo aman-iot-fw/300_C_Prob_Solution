e697d8ee152735966833a92b024f1d29cd5b6872
aman-iot-fw <aman.12006417@gmail.com>
2026-02-02 12:15:43 +0530

    added

--- 05_linked_list/basic/02_find_length_list.c
+++ 05_linked_list/basic/02_find_length_list.c
@@ -1,1 +1,69 @@
-// TODO: Implement linked list logic
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node *next;
+} Node;
+
+// Create a new node
+Node *createNode(int data) {
+    Node *tempNode = (Node *)malloc(sizeof(Node));
+    if (tempNode == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(1);  
+    }
+    tempNode->data = data;
+    tempNode->next = NULL;
+    return tempNode;
+}
+
+// Insert at end 
+void insertAtEnd(Node **head, int data) {
+    Node *newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    Node *temp = *head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+// Find length 
+int findLength(Node *head) {
+    int count = 0;  
+    Node *tempNode = head;
+    while (tempNode != NULL) {
+        count++;
+        tempNode = tempNode->next;
+    }
+    return count;
+}
+
+int main() {
+    Node *head = NULL;  // Start empty
+
+    // Build list using insert
+    insertAtEnd(&head, 1);
+    insertAtEnd(&head, 2);
+    insertAtEnd(&head, 3);
+    insertAtEnd(&head, 4);
+    insertAtEnd(&head, 5);
+
+    // Get and print length
+    int length = findLength(head);
+    printf("Length is %d\n", length);
+
+    // Free the list
+    Node *current = head;
+    while (current != NULL) {
+        Node *temp = current;
+        current = current->next;
+        free(temp);
+    }
+
+    return 0;
+}
