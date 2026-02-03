8c6f74fa6ca8b13a2d0ed98b23e7cf402e775e71
aman-iot-fw <aman.12006417@gmail.com>
2026-02-03 11:49:41 +0530

    solution added

--- 01_strings/basic/02_string_to_integer.c
+++ 01_strings/basic/02_string_to_integer.c
@@ -1,1 +1,14 @@
-#include
+#include<stdio.h>
+#include<string.h>
+
+char *str = "1234";
+
+int main(){
+    int len = strlen(str);
+    for(int i = 0; i < len; i++){
+        if(str[i] > '0' && str[i] < '9'){
+            printf("%d", str[i] - '0');
+        }
+    }
+    return 0;
+}
