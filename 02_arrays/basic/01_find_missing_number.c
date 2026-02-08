dfd6818d1d4747dbf0388de0ec310ae2d9b63993
Aman Yadav <aman.12006417@gmail.com>
2026-02-08 21:33:10 +0530

    Update 01_find_missing_number.c

--- 02_arrays/basic/01_find_missing_number.c
+++ 02_arrays/basic/01_find_missing_number.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <stdint.h>
 
-uint8_t arr[] = {1,2,4,6,7,9};
+int arr[] = {1,2,4,6,7,9};
 
 int main(){
     size_t size = sizeof(arr)/sizeof(arr[0]);
 
-    for(uint8_t i = 0; i < size - 1; i++){
+    for(int i = 0; i < size - 1; i++){
         if(arr[i+1] - arr[i] != 1){
             printf("Missing number %d\n", arr[i]+1);
         }
