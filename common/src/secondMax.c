fd6dc4c931a09ce79ceb3ae36df40857a61fdf26
Aman Yadav <aman.12006417@gmail.com>
2026-02-21 09:09:20 +0530

    Update secondMax.c

--- common/src/secondMax.c
+++ common/src/secondMax.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <limits.h>
+int main(){
+    int arr[5] = {5,5,5,5,5};
+    int max =INT_MIN;
+    int secondMax=INT_MIN;
 
+    for(uint8_t i =0; i< 5;i++){
+        if(arr[i] > max){
+            secondMax = max;
+            max = arr[i];
+        }
+        else if(arr[i] > secondMax && arr[i] != max){
+            secondMax = arr[i];
+        }
+    }
+
+    if(secondMax == INT_MIN)
+    printf("No distinct second largest");
+    else
+    printf("%d", secondMax);
+}
