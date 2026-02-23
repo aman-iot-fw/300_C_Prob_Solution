70a694bc30f8cc9388bc2c17d07562c6f3896d69
aman-iot-fw <aman.12006417@gmail.com>
2026-02-23 16:55:36 +0530

    solution added

--- 01_strings/basic/01_print_duplicate_characters.c
+++ 01_strings/basic/01_print_duplicate_characters.c
@@ -1,20 +1,8 @@
-<<<<<<< HEAD
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
-=======
->>>>>>> 27cc6539fa3eaa2f26e97b7f01ea083fcd315066
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
 
 uint8_t freq[256] = {0};
-<<<<<<< HEAD
 
 int main(){
     char *str = "Helloo";
@@ -27,25 +15,7 @@ int main(){
     for(uint8_t i = 0; i < 255; i++){
         if(freq[i]>1){
             printf("%c", i);
-=======
-char *str = "Helloo";
-
-int main(){
-    uint8_t len = strlen(str);
-    
-    for(uint8_t i = 0; i < len; i++){
-        freq[str[i]]++;
-    }
-
-    for(uint8_t i = 0; i < 255; i++){
-        if(freq[i] > 1){
-            printf("%c ", i);
->>>>>>> 27cc6539fa3eaa2f26e97b7f01ea083fcd315066
         }
     }
     return 0;
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> 27cc6539fa3eaa2f26e97b7f01ea083fcd315066
--- 01_strings/basic/03_first_non_repeated_character.c
+++ 01_strings/basic/03_first_non_repeated_character.c
@@ -1,1 +1,19 @@
-// TODO
+#include <stdio.h>
+#include <string.h>
+
+char *str = "swiss";
+unsigned char freq[256] = {0};
+
+int main(){
+    for(unsigned char i = 0; str[i] != '\0'; i++){
+        freq[str[i]]++;
+    }
+    
+    for(unsigned char i = 0; str[i] != '\0'; i++){
+        if(freq[str[i]] == 1){
+            printf("First non repeated char : %c\n", str[i]);
+            break;
+        }
+    }
+
+}
