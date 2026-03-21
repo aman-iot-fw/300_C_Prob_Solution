1c7cd5c3888e7589477c498b9d1e0c9bbf45190c
Aman Yadav <aman.12006417@gmail.com>
2026-03-21 14:02:38 +0530

    Update reverseBits.c

--- common/src/reverseBits.c
+++ common/src/reverseBits.c
@@ -1,1 +1,21 @@
+#include<stdio.h>
+
+unsigned int reverse(unsigned int n){
+    unsigned int rev = 0;
+    for(int i = 0; i < 32; i++){
+        rev <<= 1;
+        rev |= (n & 1);
+        n >>= 1;
+    }
+    return rev;
+}
+
+int main(){
+  int value = 133;
+  printf("%08b", value);
+  printf("\n%08b", reverse(value));
+
+return 0;
+}
+
 
