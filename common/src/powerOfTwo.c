c8afc2bf2b9db714d0fb060bd9f3d348dc22324a
Aman Yadav <aman.12006417@gmail.com>
2026-03-21 13:12:03 +0530

    Update powerOfTwo.c

--- common/src/powerOfTwo.c
+++ common/src/powerOfTwo.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
+#include<stdint.h>
+#include<stdbool.h>
 
+bool isPowOfTwo(int value){
+  return ((value > 0) && (value & (value-1)) == 0);
+}
+
+int main(){
+
+  int value = 1024;
+  if(isPowOfTwo(value))
+    printf("Input is Power of Two");
+
+  else
+    printf("Input is not power of Two");
+}
