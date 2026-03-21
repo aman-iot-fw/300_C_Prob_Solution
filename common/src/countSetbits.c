07c514fa7418ef0f5b802d2227eaf1746dac1f1c
Aman Yadav <aman.12006417@gmail.com>
2026-03-21 12:47:57 +0530

    Update countSetbits.c

--- common/src/countSetbits.c
+++ common/src/countSetbits.c
@@ -1,1 +1,13 @@
+#include<stdio.h>
+#include<stdint.h>
 
+uint8_t value = 0b01010111;
+
+int main(){
+  int count = 0;
+  while(value){
+    value &= (value - 1);
+    count++;
+  }
+printf("%d", count);
+}
