13c02e044db4e1da16a48254438bcf81ff2eccff
Aman Yadav <aman.12006417@gmail.com>
2026-03-21 13:40:44 +0530

    Update posFirstSetBit.c

--- common/src/posFirstSetBit.c
+++ common/src/posFirstSetBit.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
+
+int firstSetBit(int n){
+  int pos = 1;
+  while(!(n & 1)){
+    n >>= 1;
+    pos++;
+  }
+return pos;
+}
+
+int main(){
+  int value = 16;
+  printf("%d", firstSetBit(value));
+
+return 0;
+}
 
