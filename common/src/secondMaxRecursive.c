a78c45d37d40bdd9906499e42725bdc2820672f2
Aman Yadav <aman.12006417@gmail.com>
2026-03-23 08:30:35 +0530

    Update secondMaxRecursive.c

--- common/src/secondMaxRecursive.c
+++ common/src/secondMaxRecursive.c
@@ -1,1 +1,34 @@
+#include<stdio.h>
+#include<limits.h>
 
+void findSecondMax(int arr[], int size, int index, int *secondMax, int *max){
+  if(size == index)
+    return;
+
+  if(arr[index] > *max){
+    *secondMax = *max;
+    *max = arr[index];
+  }
+  else if(arr[index] > *secondMax && arr[index] != *max)
+    *secondMax = arr[index];
+
+  //recursive call
+  findSecondMax(arr, size, index+1, secondMax, max);
+}
+
+int main(){
+  int arr[] = {5,6,2,7,8,3};
+  int size = sizeof(arr)/sizeof(arr[0]);
+
+  int secondMax = INT_MIN, max = INT_MIN;
+
+  findSecondMax(arr, size, 0, &secondMax, &max);
+
+  if(secondMax == INT_MIN)
+    printf("No Second Max Found");
+  else
+    printf("SecondMax Is : %d", secondMax);
+
+  return 0;
+  
+}
