28c9bf3a15d13f40da8824420f36a73dca2f2a70
Aman Yadav <aman.12006417@gmail.com>
2026-04-04 22:48:47 +0530

    Update circularBuffer.c

--- common/src/circularBuffer.c
+++ common/src/circularBuffer.c
@@ -1,1 +1,53 @@
+#include<stdio.h>
+#include<time.h> 
+#include<stdlib.h>
 
+
+#define SIZE 10
+
+typedef struct{
+    int buffer[SIZE];
+    int head;
+    int count;
+}circularBuffer_t;
+
+void initCB(circularBuffer_t *cb){
+    cb->head = 0;
+    cb->count = 0;
+}
+
+void insertCB(circularBuffer_t *cb, int data){
+    cb->buffer[cb->head] = data;
+    
+    cb->head = (cb->head + 1) % SIZE;
+    
+    if(cb->count < SIZE){
+        cb->count++;
+    }
+}
+
+void printCB(circularBuffer_t *cb){
+    printf("Buffer : ");
+    
+    for(int i = 0; i < cb->count; i++){
+        printf("%d ", cb->buffer[i]);
+    }
+    
+    printf("\n");
+    
+}
+
+
+int main(){
+    circularBuffer_t *cb = malloc(sizeof(circularBuffer_t));
+    
+    srand(1);
+    initCB(cb);
+    
+    while(1){
+    insertCB(cb, rand() % 100);
+    printCB(cb);    
+    }
+    
+    return 0;
+}
