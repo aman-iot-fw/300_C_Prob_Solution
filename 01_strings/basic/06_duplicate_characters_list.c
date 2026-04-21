c5238f9f538a40bce1819a9513cf272727c53c74
Aman Yadav <aman.12006417@gmail.com>
2026-04-21 10:04:50 +0530

    Update 06_duplicate_characters_list.c

--- 01_strings/basic/06_duplicate_characters_list.c
+++ 01_strings/basic/06_duplicate_characters_list.c
@@ -1,1 +1,24 @@
-// TODO
+#include <stdio.h>
+#include <string.h>
+
+void findDuplicate(char *str){
+    int freq[256] = {0};
+    
+    for(int i = 0; i < strlen(str); i++){
+        freq[str[i]]++;
+    }
+    for(int i = 0; i < 256; i++){
+        if(freq[i] > 1){
+            printf("%c ", i);
+        }
+    }
+}
+
+int main()
+{
+    char *str = "Helloo";
+    findDuplicate(str);
+
+    return 0;
+}
+
