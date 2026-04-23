09cdb3e2a480908fe1a1d169d9cedb56d59b301f
Aman Yadav <aman.12006417@gmail.com>
2026-04-23 23:36:00 +0530

    Update 07_balanced_parentheses.c

--- 01_strings/advanced/07_balanced_parentheses.c
+++ 01_strings/advanced/07_balanced_parentheses.c
@@ -1,1 +1,30 @@
-// TODO
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool isParanBalenced(char *str){
+  char freq[256] = {0};
+  for(int i = 0; i < strlen(str); i++){
+      if(str[i] == '(')
+        freq[str[i]]++;
+  }
+  for(int i = 0; i < strlen(str); i++){
+      if(str[i] == ')')
+        freq[str[i]]--;
+  }
+  for(int i = 0; i < 256; i++){
+      if(freq[i] != 0)
+        return 0;
+  }
+  return 1;
+}
+
+int main(){
+  char *str = "[{())}]";
+  isParanBalenced ? printf("The string has balanced paranthesis") : printf("The string does'nt has balanced paranthesis");
+
+  return 0;
+}
+
+
+      
