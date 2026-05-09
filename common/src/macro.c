a6a9da8f91cd7f6f27e3319505cc761060710973
Aman Yadav <aman.12006417@gmail.com>
2026-05-09 23:35:42 +0530

    Update macro.c

--- common/src/macro.c
+++ common/src/macro.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "main.h"
 
+#define TOGGLE(REGISTER, POSITION) REGISTER ^ (1 << POSITION)
+#define SETBIT(REGISTER, POSITION) REGISTER |= (1 << POSITION)
+#define CLEARBIT(REGISTER, POSITION) REGISTER &= ~(1 << POSITION)
+
+#define GREATER(A, B) (A > B ? A : B)
+
+
+int main()
+{
+    uint8_t a = 0;
+    printf("%d",GREATER(1, 7));
+
+    return 0;
+}
