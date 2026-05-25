57a9d3b38f3db540d12c094d9c5c0f76c36eaa6e
aman-iot-fw <aman.12006417@gmail.com>
2026-05-25 14:59:36 +0530

    String solution added

--- 01_strings/basic/08_count_character_occurrence.c
+++ 01_strings/basic/08_count_character_occurrence.c
@@ -1,1 +1,24 @@
-// TODO
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+char *str = "Listen to your instinct";
+uint8_t freq[256] = {0};
+uint8_t count = 0;
+
+int main(){
+    int len = strlen(str);
+
+    for(int i = 0; i < len; i++){
+        if(str[i] >= 'a' && str[i] <= 'z' || str[i] >= 'A' && str[i] <= 'Z')
+            freq[str[i]]++; 
+    }
+
+    for(int i = 0; i < 256; i++){
+        if(freq[i] > 1 ){
+            printf("char : %c : count : %d\n", i, freq[i]);
+        }
+    }
+    return 0;
+
+}
