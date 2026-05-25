a3a2b4b2137e2cde09ffa26310bff2327a71e6c9
aman-iot-fw <aman.12006417@gmail.com>
2026-05-26 00:09:52 +0530

    array solution added

--- 02_arrays/basic/03_find_min_max_unsorted.c
+++ 02_arrays/basic/03_find_min_max_unsorted.c
@@ -1,1 +1,23 @@
-// TODO
+#include <stdio.h>
+#include <stdint.h>
+#include <limits.h>
+
+const int arr[] = {1,3,56,123,532, 8};
+int min = arr[0];
+int max = arr[0];
+
+int main(){
+    int size = sizeof(arr)/ sizeof(arr[0]);
+    for(int i = 0; i < size; i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+        else if(arr[i] < min)
+            min = arr[i];
+    }
+    
+    printf("MIN : %d MAX : %d", min, max);
+    return 0;
+}
+
+
--- 02_arrays/basic/04_find_pairs_with_given_sum.c
+++ 02_arrays/basic/04_find_pairs_with_given_sum.c
@@ -1,1 +1,20 @@
-// TODO
+#include<stdio.h>
+
+int arr[] = {1,2,3,4,5,6,7,8,9,0};
+
+void findPairWithGivenSum(int arr[], int sum, int size){
+    for(int i = 0; i < size; i++){
+        for(int j = i+1; j < size; j++){
+            if((arr[i] + arr[j] == sum)){
+                printf("Sum matched by adding element of index %d and %d", i, j);
+            }
+        }
+    }
+    printf("Sum not found on any indexes calculating");
+}
+
+int main(){
+    int size = sizeof(arr)/sizeof(arr[0]);
+    findPairWithGivenSum(arr,5,size);
+    return 0;
+}
