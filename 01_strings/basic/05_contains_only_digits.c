4ef5b25a0d4fb93d7433f9128a5cbbc24b5e87e7
aman-iot-fw <aman.12006417@gmail.com>
2026-05-25 14:31:11 +0530

    String solution added

--- 01_strings/basic/05_contains_only_digits.c
+++ 01_strings/basic/05_contains_only_digits.c
@@ -1,1 +1,17 @@
-// TODO
+#include <stdio.h>
+#include <string.h>
+
+char *str = "12349>0";
+
+int main(){
+
+    for(int i = 0; i < strlen(str); i++){
+        if(str[i] < '0' || str[i] > '9'){
+            printf("String contain other than digit");
+            return 1;
+        }
+    }
+    printf("String contains digit only");
+    return 0;
+
+}
