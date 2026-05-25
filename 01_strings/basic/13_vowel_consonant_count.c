852654ac23cb15d035fb038bc787cdc643a5426f
aman-iot-fw <aman.12006417@gmail.com>
2026-05-25 17:10:23 +0530

    a

--- 01_strings/basic/13_vowel_consonant_count.c
+++ 01_strings/basic/13_vowel_consonant_count.c
@@ -1,1 +1,33 @@
-// TODO
+#include <stdio.h>
+#include <string.h>
+
+char str[] = "I'm a firmware and embedded software engineer";
+int vovelCount = 0;
+int consoentCount = 0;
+
+int main(){
+    int len = strlen(str);
+    for(int i = 0; i < len; i++){
+        if((str[i] >= 'A' && str[i] <= 'Z') ||
+           (str[i] >= 'a' && str[i] <= 'z'))
+           {
+            if( str[i] =='A'
+            ||  str[i] =='E'
+            ||  str[i] =='I'
+            ||  str[i] =='O'
+            ||  str[i] =='U'
+            ||  str[i] =='a'
+            ||  str[i] =='e'
+            ||  str[i] =='i'
+            ||  str[i] =='o'
+            ||  str[i] =='u')
+                vovelCount++;
+            else 
+                consoentCount++;
+            }
+
+        
+    }
+    printf("Vovel Count : %d Consonet Count : %d", vovelCount, consoentCount);
+    return 0;
+}
