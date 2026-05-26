f238a5d8c7e5e15ccbad7b4e0cf72950e3d7edd6
Aman Yadav <aman.12006417@gmail.com>
2026-05-26 14:41:33 +0530

    Update AES128.c

--- common/AES128.c
+++ common/AES128.c
@@ -1,1 +1,41 @@
+#include <stdio.h>
+#include <stdint.h>
 
+#define AES256_KYE_SIZE 32
+#define AES128_KYE_SIZE 16
+#define BLOCK_SIZE      16
+
+
+uint8_t *plainText = "EmbeddedEngineer";
+uint8_t *key = "EngineerEmbedded";
+uint8_t encryptedText[BLOCK_SIZE];
+uint8_t output[BLOCK_SIZE];
+
+void aes128encrypt(uint8_t *plainText, uint8_t *key, uint8_t *encryptedText){
+  for(int i = 0; i < BLOCK_SIZE; i++){
+    encryptedText[i] = plainText[i] ^ key[i];
+  }
+}
+
+void aes128decrypt(uint8_t *output, uint8_t *key, uint8_t *encryptedText){
+  for(int i = 0; i < BLOCK_SIZE; i++){
+    output[i] = encryptedText[i] ^ key[i];
+  }
+}
+
+void printBuffer(uint8_t *title, uint8_t *data, int size){
+  printf("%s : ",title);
+  for(int i = 0; i < size; i++){
+    printf("%02X ", data[i]);
+  }
+  printf("\n");
+}
+
+int main(){
+  printBuffer("Plain     Text", plainText, BLOCK_SIZE);
+  aes128encrypt(plainText, key, encryptedText);
+  printBuffer("Encrypted Text", encryptedText, BLOCK_SIZE);
+  aes128decrypt(output, key, encryptedText);
+  printBuffer("Decrypted Text", output, BLOCK_SIZE);
+  return 0;
+}
