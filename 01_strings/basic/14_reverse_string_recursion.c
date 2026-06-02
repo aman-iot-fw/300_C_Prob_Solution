080cd7947729b94b3396cc0a4a3ef6eb93ae1612
Aman Yadav <aman.12006417@gmail.com>
2026-06-02 16:22:48 +0530

    Add recursive function to reverse words in a string
    Implemented a recursive function to reverse words in a string.

--- 01_strings/basic/14_reverse_string_recursion.c
+++ 01_strings/basic/14_reverse_string_recursion.c
@@ -1,1 +1,31 @@
-// TODO
+#include <stdio.h>
+
+void reverseWords(char *str)
+{
+    if(*str == '\0')
+        return;
+
+    char *space = str;
+
+    while(*space && *space != ' ')
+        space++;
+
+    if(*space)
+    {
+        reverseWords(space + 1);
+        printf("%.*s ", (int)(space - str), str);
+    }
+    else
+    {
+        printf("%s ", str);
+    }
+}
+
+int main()
+{
+    char str[] = "hey how are you";
+
+    reverseWords(str);
+
+    return 0;
+}
