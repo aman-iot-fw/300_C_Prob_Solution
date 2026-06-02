0b1fa6ba91dd8573465b5cfbd53ad731bd80b432
Aman Yadav <aman.12006417@gmail.com>
2026-06-02 16:17:07 +0530

    Add palindrome check function in C

--- 01_strings/basic/12_check_palindrome.c
+++ 01_strings/basic/12_check_palindrome.c
@@ -1,1 +1,29 @@
 
+#include<stdio.h>
+#include<string.h>
+#include<stdbool.h>
+
+
+char str[] = "madam";
+
+bool isStrPalindrome(char *str){
+    int left = 0;
+    int right = strlen(str) - 1;
+    while(left < right){
+        if(str[left] != str[right]){
+            return false;
+        }
+        
+        left++;
+        right--;
+    }
+    return true;
+}
+
+int main(){
+
+ isStrPalindrome(str) ? printf("String a palindrome") :  printf("String is not palindrome");
+ 
+  return 0;
+}
+
