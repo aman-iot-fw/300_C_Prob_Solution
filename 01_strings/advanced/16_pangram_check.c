b8dd6560a08238335f21360c9f48d2364fd9c646
Aman Yadav <aman.12006417@gmail.com>
2026-06-03 19:38:19 +0530

    Implement pangram check in C

--- 01_strings/advanced/16_pangram_check.c
+++ 01_strings/advanced/16_pangram_check.c
@@ -1,1 +1,30 @@
-// TODO
+#include<stdio.h>
+#include<string.h>
+#include<stdint.h>
+#include<stdbool.h>
+
+char str[] = "The quick brown fox jumps over a lazy dog";
+
+bool isPanagram(char *str){
+  int len = strlen(str);
+  uint8_t freq[256] = {0};
+
+  for(int i = 0; i < len; i++){
+    if(str[i] >= 'A' && str[i] <= 'Z')
+      str[i] = str[i] + 32;
+    freq[str[i]]++;
+  }
+  for(int i = 'a'; i <= 'z'; i++){
+    if(freq[i] < 1)
+      return 0;
+  }
+  return 1;
+  
+}
+
+
+int main(){
+
+  isPanagram(str) ? printf("True") : printf("False");
+  return 0;
+}
