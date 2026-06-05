8e53c0adf648b78caba7403244558be9670f18e2
Aman Yadav <aman.12006417@gmail.com>
2026-06-05 13:51:36 +0530

    Update 04_swap_strings_double_pointer.c

--- 03_pointers/basic/04_swap_strings_double_pointer.c
+++ 03_pointers/basic/04_swap_strings_double_pointer.c
@@ -1,1 +1,26 @@
-// TODO: Implement pointer-based solution
+#include<stdio.h>
+#include<string.h>
+#include<stdint.h>
+
+void swapString(char **str1, char **str2){
+  char *tmp;
+  tmp = *str1;
+  *str1 = *str2;
+  *str2 = tmp;
+}
+
+int main(){
+  char *str1 = "Hello";
+  char *str2 = "World";
+  printf("Before Swap\n");
+  printf("%s\n", str1);
+  printf("%s\n", str2);
+
+  swapString(&str1, &str2);
+
+  printf("\nAfter Swap\n");
+  printf("%s\n", str1);
+  printf("%s\n", str2);
+  
+  return 0;
+}
