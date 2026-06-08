39393b51c55f7800056f4dee9ad993f46ac42c3f
Aman Yadav <aman.12006417@gmail.com>
2026-06-08 14:36:50 +0530

    Update 08_array_intersection.c

--- 02_arrays/basic/08_array_intersection.c
+++ 02_arrays/basic/08_array_intersection.c
@@ -1,1 +1,27 @@
+#include<stdio.h>
+#include<stdint.h>
+
+int removeDup(int arr[], int size){
+  uint8_t seen[256] = {0};
+  int write = 0;
+  for(int i = 0; i < size; i++){
+    if(!seen[arr[i]]){
+      seen[arr[i]] = 1;
+      arr[write] = arr[i];
+      write++;
+    }
+  }
+  return write;
+}
+
+int main(){
+  int arr[] = {1, 1, 2, 3, 5, 3, 5, 6, 7, 8, 1};  
+  int size = sizeof arr/ sizeof arr[0];
+  int newSize = removeDup(arr, size);
+
+  for(int i = 0; i < newSize; i++){
+    printf("%d ", arr[i]);
+  }
+  return 0;
+}
 
