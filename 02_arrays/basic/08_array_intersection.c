34883d9af44c4b39a650fc2af4a687bc9b508f71
Aman Yadav <aman.12006417@gmail.com>
2026-06-08 14:48:13 +0530

    Update 08_array_intersection.c

--- 02_arrays/basic/08_array_intersection.c
+++ 02_arrays/basic/08_array_intersection.c
@@ -1,27 +1,24 @@
 #include<stdio.h>
 #include<stdint.h>
 
-int removeDup(int arr[], int size){
-  uint8_t seen[256] = {0};
-  int write = 0;
-  for(int i = 0; i < size; i++){
-    if(!seen[arr[i]]){
-      seen[arr[i]] = 1;
-      arr[write] = arr[i];
-      write++;
+void intersection(int arr1[], int size1, int arr2[], int size2){
+  printf("Intersection : ");
+  for(int i = 0; i < size1; i++){
+   for(int j = 0; j < size2; j++){
+     if(arr1[i] == arr2[j]){
+       printf("%d ", arr1[i]);
+       break;
+     }
     }
   }
-  return write;
 }
 
 int main(){
-  int arr[] = {1, 1, 2, 3, 5, 3, 5, 6, 7, 8, 1};  
-  int size = sizeof arr/ sizeof arr[0];
-  int newSize = removeDup(arr, size);
-
-  for(int i = 0; i < newSize; i++){
-    printf("%d ", arr[i]);
-  }
+  int arr1[] = {1,2,4,6,7,3};  
+  int size1 = sizeof arr1/ sizeof arr1[0];
+    int arr2[] = {2,5,7,8,3,1,2,9,6};  
+  int size2 = sizeof arr2/ sizeof arr2[0];
+  intersection(arr1, size1, arr2, size2);
   return 0;
 }
 
