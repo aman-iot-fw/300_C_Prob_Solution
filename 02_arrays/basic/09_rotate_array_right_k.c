d81795ded551aff1684e2621f5158202017c9829
Aman Yadav <aman.12006417@gmail.com>
2026-06-08 15:09:40 +0530

    Update 09_rotate_array_right_k.c

--- 02_arrays/basic/09_rotate_array_right_k.c
+++ 02_arrays/basic/09_rotate_array_right_k.c
@@ -1,1 +1,29 @@
+#include<stdio.h>
+#include<stdint.h>
 
+void rev(int arr[], int left, int right){
+    while(left < right){
+        int tmp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = tmp;
+        
+        left++;
+        right--;
+    }
+}
+
+void rotateArrRightK(int arr[], int size, int k){
+    rev(arr, 0, size - 1);
+    rev(arr, 0, k - 1);
+    rev(arr, k, size - 1);
+}
+
+int main(){
+  int arr[] = {1,2,3,4,5,6,7};
+  int size = sizeof arr/ sizeof arr[0];
+  rotateArrRightK(arr, size, 2);
+  for(int i = 0; i < size; i++){
+    printf("%d ", arr[i]);
+  }
+  return 0;
+}
