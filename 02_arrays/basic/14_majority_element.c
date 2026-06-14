c48fe9455ddff73ea9930c412879e923f61b6e07
Aman Yadav <aman.12006417@gmail.com>
2026-06-14 23:14:24 +0530

    Update 14_majority_element.c

--- 02_arrays/basic/14_majority_element.c
+++ 02_arrays/basic/14_majority_element.c
@@ -1,1 +1,32 @@
-// TODO
+#include <stdio.h>
+
+int majorityElement(int arr[], int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        int count = 0;
+
+        for(int j = 0; j < size; j++)
+        {
+            if(arr[i] == arr[j])
+                count++;
+        }
+
+        if(count > size / 2)
+            return arr[i];
+    }
+
+    return -1;
+}
+
+int main()
+{
+    int arr[] = {3,3,4,2,4,4,2,4,4};
+
+    int size = sizeof(arr)/sizeof(arr[0]);
+
+    printf("Majority Element = %d\n",
+           majorityElement(arr, size));
+
+    return 0;
+}
