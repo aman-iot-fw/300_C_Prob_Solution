39cd9af7a6b66729b394a59aa61b151eba0b498e
Aman Yadav <aman.12006417@gmail.com>
2026-06-14 23:08:43 +0530

    Update 12_move_zeros_to_left.c

--- 02_arrays/basic/12_move_zeros_to_left.c
+++ 02_arrays/basic/12_move_zeros_to_left.c
@@ -1,1 +1,42 @@
-//move zeros to left
+#include <stdio.h>
+
+void moveZerosLeft(int arr[], int size)
+{
+    int write = size - 1;
+
+    for(int i = size - 1; i >= 0; i--)
+    {
+        if(arr[i] != 0)
+        {
+            arr[write] = arr[i];
+            write--;
+        }
+    }
+
+    while(write >= 0)
+    {
+        arr[write] = 0;
+        write--;
+    }
+}
+
+void printArray(int arr[], int size)
+{
+    for(int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+
+    printf("\n");
+}
+
+int main()
+{
+    int arr[] = {1,2,0,4,3,0,5,0};
+
+    int size = sizeof(arr)/sizeof(arr[0]);
+
+    moveZerosLeft(arr, size);
+
+    printArray(arr, size);
+
+    return 0;
+}
