c664c4d45ccdffa8cf9b89157bb1607f8614c05c
Aman Yadav <aman.12006417@gmail.com>
2026-06-16 14:15:52 +0530

    Update 06_concatenate_strings.c

--- 03_pointers/basic/06_concatenate_strings.c
+++ 03_pointers/basic/06_concatenate_strings.c
@@ -1,1 +1,36 @@
-// TODO: Implement pointer-based solution
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+char str1[20] = "Hello";
+char *str2 = "World";
+
+void concatenate(char *str1, char *str2){
+    while(*str1){
+        str1++;
+    }
+    *str1 = ' ';
+    str1++;
+    while(*str2){
+        *str1 = *str2;
+        
+        str1++;
+        str2++;
+    }
+    
+    *str1 = '\0';
+}
+
+int main()
+{
+    concatenate(str1, str2);
+    printf("%s", str1);
+
+    return 0;
+}
