116caada587d87505131409fe71d158fec0b8245
Aman Yadav <aman.12006417@gmail.com>
2026-06-16 15:07:57 +0530

    Update 07_split_string_halves.c

--- 03_pointers/basic/07_split_string_halves.c
+++ 03_pointers/basic/07_split_string_halves.c
@@ -1,1 +1,25 @@
-// TODO: Implement pointer-based solution
+#include<stdio.h>
+
+char *str = "Hello World";
+char str1[10];
+char str2[10];
+
+void halfString(char *str){
+  while(*str){
+    *str1 = *str;
+    str++;
+    str1++;
+  }
+  while(*str){
+    *str2 = *str;
+    str++;
+    str2++;
+  }
+} 
+
+int main(){
+  halfString(str);
+  printf("First half : %s\nSecond half : %s", str1, str2);
+  return 0;
+  
+}
