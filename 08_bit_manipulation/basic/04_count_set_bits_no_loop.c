86970d8d53f60daef9dfbfe6ccb4fc111bf86f79
Aman Yadav <aman.12006417@gmail.com>
2026-06-17 16:32:36 +0530

    Update 04_count_set_bits_no_loop.c

--- 08_bit_manipulation/basic/04_count_set_bits_no_loop.c
+++ 08_bit_manipulation/basic/04_count_set_bits_no_loop.c
@@ -1,1 +1,16 @@
-// TODO: Implement bit manipulation logic
+#include<stdio.h>
+#include<stdint.h>
+
+uint8_t countSetBit(uint8_t reg){
+  uint8_t count = 0;
+  if(reg == 0)
+    return 0;
+  return 1 + countSetBit(reg & (reg - 1));
+}
+
+int main(){
+  uint8_t reg = 128;
+  printf("Count of set bit : %d", countSetBit(reg)  );
+  return 0;
+}
+
