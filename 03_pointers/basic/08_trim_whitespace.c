a56123b0ecbe1836289748b62adf7a4a57553c3c
Aman Yadav <aman.12006417@gmail.com>
2026-06-17 10:58:36 +0530

    Update 08_trim_whitespace.c

--- 03_pointers/basic/08_trim_whitespace.c
+++ 03_pointers/basic/08_trim_whitespace.c
@@ -1,1 +1,39 @@
-// TODO: Implement pointer-based solution
+#include <stdio.h>
+
+void trimWhitespace(char *str)
+{
+    char *start = str;
+    char *end = str;
+
+    /* Find first non-space */
+    while(*start == ' ')
+        start++;
+
+    while(*end)
+        end++;
+
+    end--;
+
+    while(end > start && *end == ' ')
+        end--;
+
+    char *dst = str;
+
+    while(start <= end)
+    {
+        *dst++ = *start++;
+    }
+
+    *dst = '\0';
+}
+
+int main()
+{
+    char str[] = "   Hello World   ";
+
+    trimWhitespace(str);
+
+    printf("%s\n", str);
+
+    return 0;
+}
