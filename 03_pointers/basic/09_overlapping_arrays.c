d0bf1b27bab9ecfc3c4002fc9946067b9cd6cfe4
Aman Yadav <aman.12006417@gmail.com>
2026-06-17 11:00:26 +0530

    Update 09_overlapping_arrays.c

--- 03_pointers/basic/09_overlapping_arrays.c
+++ 03_pointers/basic/09_overlapping_arrays.c
@@ -1,1 +1,43 @@
-// TODO: Implement pointer-based solution
+#include <stdio.h>
+
+void findOverlap(int *arr1, int size1,
+                 int *arr2, int size2)
+{
+    int *p1 = arr1;
+    int *p2 = arr2;
+
+    while(p1 < arr1 + size1 &&
+          p2 < arr2 + size2)
+    {
+        if(*p1 < *p2)
+        {
+            p1++;
+        }
+        else if(*p1 > *p2)
+        {
+            p2++;
+        }
+        else
+        {
+            printf("%d ", *p1);
+
+            p1++;
+            p2++;
+        }
+    }
+}
+
+int main()
+{
+    int arr1[] = {1,2,3,4,5,6};
+    int arr2[] = {5,6,7,8};
+
+    int size1 = sizeof(arr1)/sizeof(arr1[0]);
+    int size2 = sizeof(arr2)/sizeof(arr2[0]);
+
+    printf("Overlapping Elements: ");
+
+    findOverlap(arr1, size1, arr2, size2);
+
+    return 0;
+}
