b4ee6ccba3dd935952ddd3a58905644edeab58da
Aman Yadav <aman.12006417@gmail.com>
2026-06-17 16:13:23 +0530

    Update 03_power_of_two_check.c

--- 08_bit_manipulation/basic/03_power_of_two_check.c
+++ 08_bit_manipulation/basic/03_power_of_two_check.c
@@ -1,1 +1,12 @@
-// TODO: Implement bit manipulation logic
+#include<stdio.h>
+#include<stdint.h>
+
+uint8_t powOfTwo(uint8_t reg){
+  return ((reg > 0) && (reg & reg - 1) == 0);
+}
+
+int main(){
+  uint8_t reg = 128;
+  powOfTwo(reg) ? printf("Number is power of two") : printf("Number is not power of two");
+  return 0;
+}
