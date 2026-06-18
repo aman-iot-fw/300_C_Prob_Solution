20a07e3393f1a74eb465fe4c9f1de09ce5401352
Aman Yadav <aman.12006417@gmail.com>
2026-06-18 20:06:13 +0530

    Update 05_swap_odd_even_bits.c

--- 08_bit_manipulation/basic/05_swap_odd_even_bits.c
+++ 08_bit_manipulation/basic/05_swap_odd_even_bits.c
@@ -1,1 +1,19 @@
-// TODO: Implement bit manipulation logic
+#include<stdio.h>
+#include<stdint.h>
+
+uint8_t swapOddEven(uint8_t reg){
+    uint8_t oddBits  = reg & 0b10101010;
+    uint8_t evenBits = reg & 0b01010101;
+    
+    oddBits  >>= 1;
+    evenBits <<= 1;
+    
+  return oddBits | evenBits;
+}
+
+int main(){
+  uint8_t reg = 0b10001011;
+  printf("Swaped Bits : %08b", swapOddEven(reg));
+  return 0;
+}
+
