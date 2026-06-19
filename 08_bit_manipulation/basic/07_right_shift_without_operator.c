b6a54dbce6ad39f28557284e8563b47aff271dd9
Aman Yadav <aman.12006417@gmail.com>
2026-06-19 09:31:41 +0530

    Update 07_right_shift_without_operator.c

--- 08_bit_manipulation/basic/07_right_shift_without_operator.c
+++ 08_bit_manipulation/basic/07_right_shift_without_operator.c
@@ -1,1 +1,14 @@
-// TODO: Implement bit manipulation logic
+#include<stdio.h>
+#include<stdint.h>
+
+uint8_t rtSftWithoutOper(uint8_t reg, uint8_t pos){
+  //righ shift is equal to divide by 2
+  //left shift is equal to multiply by 2
+
+ return reg / pos / 2;
+}
+
+int main(){
+  uint8_t reg = 0b00010100;
+  printf("%08b", rtSftWithoutOper(reg, 1));
+}
