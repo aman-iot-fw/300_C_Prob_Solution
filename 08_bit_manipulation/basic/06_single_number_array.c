2157064d1e562957381f23ce309e9a47a79a64be
Aman Yadav <aman.12006417@gmail.com>
2026-06-19 09:20:06 +0530

    Update 06_single_number_array.c

--- 08_bit_manipulation/basic/06_single_number_array.c
+++ 08_bit_manipulation/basic/06_single_number_array.c
@@ -1,1 +1,17 @@
-// TODO: Implement bit manipulation logic
+#include<stdio.h>
+
+int nonRepInArr(int arr[], int size){
+  int result = 0;
+  for(int i = 0; i < size; i++){
+    result ^= arr[i];
+  }
+  return result;
+}
+
+int main(){
+  int arr[] = {1,1,2,2,3,3,4,5,5,6,6};
+  int size = sizeof arr / sizeof arr[0];
+  printf("Non Repetative Number In The Array : %d",nonRepInArr(arr, size));
+
+  return 0;
+}
