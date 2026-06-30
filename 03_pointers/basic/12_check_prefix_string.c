656b2673e3c9d11affd098b17b6c9da1665a73fa
Aman Yadav <aman.12006417@gmail.com>
2026-06-30 17:13:11 +0530

    Update 12_check_prefix_string.c

--- 03_pointers/basic/12_check_prefix_string.c
+++ 03_pointers/basic/12_check_prefix_string.c
@@ -1,1 +1,21 @@
-// TODO: Implement pointer-based solution
+#include <stdio.h>
+
+void matchFrefix(char *str1, char *str2){
+    while(*str1){
+        if(*str1++ != *str2++){
+            printf("str1 is not the frefix of str2");
+            return;
+        }
+    }
+    printf("str1 is the frefix of str2");
+}
+
+int main(){
+  char *str1 = "h";
+  char *str2 = "hello";
+  
+  matchFrefix(str1, str2);
+
+  return 0;
+  
+}
