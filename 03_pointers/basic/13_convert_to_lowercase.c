a85e3580cb9abd8a975da1085b2eb504684a113c
Aman Yadav <aman.12006417@gmail.com>
2026-07-01 23:13:32 +0530

    Add pointer-based lowercase conversion function
    Implemented a pointer-based function to convert a string to lowercase.

--- 03_pointers/basic/13_convert_to_lowercase.c
+++ 03_pointers/basic/13_convert_to_lowercase.c
@@ -1,1 +1,21 @@
-// TODO: Implement pointer-based solution
+#include <stdio.h>
+
+void converToLowerCase(char *inp){
+    char *start = inp;
+    while(*inp != '\0'){
+        if(*inp >= 'A' && *inp <= 'Z'){
+            *inp += 32;
+        }
+        inp++;
+    }
+    printf("%s", start);
+}
+
+int main()
+{
+    char str[] = "HeLlo";
+    converToLowerCase(str);
+    
+
+    return 0;
+}
