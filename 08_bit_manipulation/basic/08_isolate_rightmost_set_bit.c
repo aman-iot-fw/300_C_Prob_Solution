a44201dfefb29adaea4823e487b308e572025965
Aman Yadav <aman.12006417@gmail.com>
2026-07-02 17:08:44 +0530

    Update 08_isolate_rightmost_set_bit.c

--- 08_bit_manipulation/basic/08_isolate_rightmost_set_bit.c
+++ 08_bit_manipulation/basic/08_isolate_rightmost_set_bit.c
@@ -1,1 +1,15 @@
-// TODO: Implement bit manipulation logic
+#include <stdio.h>
+#include <stdint.h>
+
+uin8t_t reg = 0b11011010;
+
+void isolateRightMostBit(uint8_t reg){
+  uint8_t isolated = reg;
+  isolated &= (~reg + 1);
+  printf("%08b", isolated);
+}
+
+int main(){
+  isolateRightMostBit(reg);
+  return 0;
+}
